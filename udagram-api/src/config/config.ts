cba905480ea6cccd0249493c9bfb88e00a285f3e
sarahishamsaied <sarahishamsaied@gmail.com>
2022-10-20 11:58:55 +0200

    edited package.sjon

--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -8,7 +8,7 @@ dotenv.config();
 // Are Also needed
 
 export const config = {
-  username: "postgres",
+  username: process.env.POSTGRES_USERNAME,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
   dbPort: process.env.DB_PORT,
